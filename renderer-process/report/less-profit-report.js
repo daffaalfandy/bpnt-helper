854ee78891c526563b05ab37806f6e0567660a3e
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-07-20 17:36:04 +0700

    Add export to pdf feature on profit report

--- renderer-process/report/less-profit-report.js
+++ renderer-process/report/less-profit-report.js
@@ -2,26 +2,32 @@ const { ipcRenderer, remote } = require('electron');
 const path = require('path');
 const fs = require('fs');
 const inputSection = require('../../assets/main');
-const { sign } = require('crypto');
+
+// Importing BrowserWindow from Main 
+const BrowserWindow = remote.BrowserWindow;
 
 let btnBackProfit = document.getElementById('btn-back-profit-report');
 let btnAddToPdfProfit = document.getElementById('btn-profit-pdf');
 let profitTitleField = document.getElementById('profit-title');
-let alerPdfProfit = document.getElementById('alert-profit');
+let alertPdfProfit = document.getElementById('alert-profit');
 let titleBuyProfitField = document.getElementById('buy-title-profit');
 let itemsBuyTable = document.getElementById('items-buy-table');
 let stockTable = document.getElementById('stock-field');
 let summaryTable = document.getElementById('summary');
 let sumOfBuyPrice = 0;
 let sumOfSellPrice = 0;
+let datePdf;
 
 btnBackProfit.addEventListener('click', (e) => {
     sumOfBuyPrice = 0;
     sumOfSellPrice = 0;
+    alertPdfProfit.style.display = 'none';
+    alertPdfProfit.innerHTML = '';
     inputSection.handleInputTrigger('report');
 });
 
 ipcRenderer.on('profit-report-data', (event, items) => {
+    datePdf = { month: items.itemsData[0].month, year: items.itemsData[0].year };
     sumOfBuyPrice = 0;
     sumOfSellPrice = 0;
 
@@ -107,3 +113,39 @@ function setSummaryTable(items) {
     }
     summaryTable.innerHTML = htmlResult;
 }
+
+btnAddToPdfProfit.addEventListener('click', (event) => {
+    let filepath1 = path.join(__dirname, `../../laporan/laporan-laba-rugi-${datePdf.month}-${datePdf.year}.pdf`);
+
+    let options = {
+        marginsType: 0,
+        pageSize: 'A4',
+        printBackground: true,
+        printSelectionOnly: false,
+        landscape: false
+    }
+
+    document.body.style.backgroundColor = '#fff'
+    document.getElementById('main-sidenav').style.visibility = 'hidden';
+    btnBackProfit.style.display = 'none';
+    btnAddToPdfProfit.style.display = 'none';
+    // let win = BrowserWindow.getAllWindows()[0]; 
+    let win = BrowserWindow.getFocusedWindow();
+
+    win.webContents.printToPDF(options).then(data => {
+        fs.writeFile(filepath1, data, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                document.body.style.backgroundColor = '#161E37';
+                document.getElementById('main-sidenav').style.visibility = 'visible';
+                btnBackProfit.style.display = 'inline';
+                btnAddToPdfProfit.style.display = 'inline';
+                alertPdfProfit.style.display = 'block';
+                alertPdfProfit.innerHTML = `File berhasil dibuat ${filepath1}`;
+            }
+        });
+    }).catch(error => {
+        console.log(error)
+    });
+}); 
